e9e2cbe7d7bd0f14946ad82ece8f9092b843083a
Cancelkam <Cancel.kam@gmail.com>
2020-04-14 10:53:45 +0300

    initial commit

---
+++ src/test/java/ru/netology/ConditionerTest.java
@@ -0,0 +1,45 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+@Test
+public void shouldGetAndSet() {
+    Conditioner conditioner = new Conditioner();
+    String expected = "Кондишн";
+
+    assertNull(conditioner.getName());
+    conditioner.setName(expected);
+    assertEquals(expected, conditioner.getName());
+}
+
+    @Test
+    void increaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMaxTemperature(26);
+        conditioner.setCurrentTemperature(25);
+
+        conditioner.increaseCurrentTemperature();
+
+        assertEquals(26,conditioner.getCurrentTemperature());
+        conditioner.increaseCurrentTemperature();
+        assertEquals(26,conditioner.getCurrentTemperature());
+
+    }
+
+    @Test
+    void decreaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMaxTemperature(99);
+        conditioner.setMinTemperature(10);
+        conditioner.setCurrentTemperature(11);
+        conditioner.decreaseCurrentTemperature();
+        assertEquals(10,conditioner.getCurrentTemperature());
+        conditioner.decreaseCurrentTemperature();
+        assertEquals(10,conditioner.getCurrentTemperature());
+
+
+    }
+}
